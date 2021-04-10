7eb9f0494f7acb5d034e9d0747efa8c4240e41bb
Gavin <grncheetah10@gmail.com>
2021-04-10 14:40:35 -0500

    Added framework to login/logout users

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 
@@ -14,6 +14,25 @@ def create_app():
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'main.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(username):
+        """Load user from Astra by username, load into User object
+        
+        current_user calls this function
+        """
+        user = User()
+        user.username = username
+        user.password = "test"
+        ## add all data from astra for this user
+        
+        return user
 
     return app
 
--- project/main.py
+++ project/main.py
@@ -1,15 +1,19 @@
 from flask import Blueprint, Flask, request, Response, redirect, url_for, jsonify
+from flask_login import login_user, logout_user, login_required, current_user
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 
-import db
+import db_astra
+
+from .models import User
+from . import db
 
 main = Blueprint('main', __name__)
 
 @main.route("/create_user", methods=["POST"])
 def create_user():
 
-    if db.create_user(request.form['user'], request.form['pass']):
+    if db_astra.create_user(request.form['user'], request.form['pass']):
         return jsonify("{'Success': 'The user was added to core.user.'")
 
     return jsonify("{'Failure': 'The user was not added to core.user.'")
@@ -18,9 +22,28 @@ def create_user():
 @main.route("/login", methods=["POST"])
 def login():
 
-    if db.login(request.form['user'], request.form['pass']):
+    remember = remember = True if request.form.get('remember') else False
+
+    # Checks for valid login
+    if db_astra.login(request.form['user'], request.form['pass']):
+        user_to_test = User()
+        user_to_test.username = request.form['user']
+        user_to_test.password = request.form['pass'] # Might not need
+        login_user(user_to_test, remember=remember)
+
         return jsonify("{'Success': 'The user is logged in.'")
+        #return redirect(url_for('main.profile'))
 
     return jsonify("{'Failure': 'The user was not logged in.'")
+    
 
+@main.route('/profile')
+@login_required
+def profile():
+    return current_user.username
 
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return "logged out"
