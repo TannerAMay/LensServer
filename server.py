34175a46b62fdcb75c591d56d1de5a30dad9993f
Gavin <grncheetah10@gmail.com>
2021-04-10 12:51:12 -0500

    Flask create_user start

--- server.py
+++ server.py
@@ -1,16 +1,34 @@
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 
-cloud_config = {
-        'secure_connect_bundle': '<</PATH/TO/>>secure-connect-p2021.zip'
-}
-auth_provider = PlainTextAuthProvider('<<CLIENT ID>>', '<<CLIENT SECRET>>')
+from flask import Flask, request, Response, redirect, url_for, jsonify
 
-cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
-session = cluster.connect()
 
-row = session.execute("select * from core.topic").all()
-if row:
-    print(row)
-else:
-    print("An error occurred.")
+app = Flask(__name__)
+
+@app.route("/create_user", methods=["POST"])
+def create_user():
+
+    print(request.form['user'])
+    return jsonify("{'test': 'this is test data'}")
+
+def login():
+
+    return jsonify("    ")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# cloud_config = {
+#         'secure_connect_bundle': '<</PATH/TO/>>secure-connect-p2021.zip'
+# }
+# auth_provider = PlainTextAuthProvider('<<CLIENT ID>>', '<<CLIENT SECRET>>')
+
+# cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+# session = cluster.connect()
+
+# row = session.execute("select * from core.topic").all()
+# if row:
+#     print(row)
+# else:
+#     print("An error occurred.")
