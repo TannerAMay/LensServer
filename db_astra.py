3d5581021ca5c6f4cecec25589079dd8cd8f40be
Tanner May <tamfrb@umsystem.edu>
2021-04-11 03:56:12 -0500

    Use hex for hashes

--- db_astra.py
+++ db_astra.py
@@ -30,8 +30,8 @@ def create_user(username: str, password: str, salt: bytes) -> Tuple[bool, bool]:
     addUserToUserdata = SESSION.execute(f"INSERT INTO core.userdata (username, bio, createdate) "
                                         f"VALUES ('{username}', '', '{int(time())}') "
                                         f"IF NOT EXISTS").one()
-    addUserToAuth = SESSION.execute(f"INSERT INTO auth.users (username, password, salt) "
-                                    f"VALUES ('{username}', '{hash}', {salt}) "
+    addUserToAuth = SESSION.execute(f"INSERT INTO auth.users (username, hash, hashsalt) "
+                                    f"VALUES ('{username}', '{hash.hex()}, {salt.hex()}') "
                                     f"IF NOT EXISTS").one()
 
     return addUserToUserdata[0], addUserToAuth[0]
@@ -57,7 +57,7 @@ def login(username: str, password: str) -> bool:
     )
 
     # If select returned results or more than two columns or passwords do not match, return false.
-    if cmd is None or len(cmd) != 2 or cmd[1] != hash:
+    if cmd is None or len(cmd) != 2 or cmd[1] != hash.hex():
         return False
 
     return True
